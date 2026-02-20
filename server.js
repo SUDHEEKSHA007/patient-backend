6a8663ae3f1e464c842de772e7025bb11bc8c14b
SUDHEEKSHA007 <tkjsudheeksha@gmail.com>
2026-02-20 23:03:53 +0530

    Fix render backend

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const app = express();
 // Middleware
 app.use(express.json());
 
-// Home Route (fixes Cannot GET /)
+// Home Route (Fixes Cannot GET /)
 app.get("/", (req, res) => {
-  res.send("Patient Backend API is Running Successfully");
+  res.send("Patient Backend API is Running 🚀");
 });
 
 // Sample API Route
 app.get("/patients", (req, res) => {
   res.json([
     { id: 1, name: "John", age: 22 },
-    { id: 2, name: "Sudheeksha", age: 20 }
+    { id: 2, name: "Sudheeksha", age: 19 }
   ]);
 });
 
@@ -21,5 +21,5 @@ app.get("/patients", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+  console.log(`Server running on port ${PORT}`);
 });
